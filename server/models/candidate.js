75f69ede6884c324dc10552964b356c5daf7e994
adesh.exc <bodead@rknec.edu>
2024-05-28 03:36:23 -0700

    Update candidate.js

--- server/models/candidate.js
+++ server/models/candidate.js
@@ -44,6 +44,28 @@ module.exports = Candidate;
 // below is imprved candidateSchema as there can be many voter it is better to create a diiferent schema for votes
 
 
+const candidateSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    party: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    voteCount: {
+        type: Number,
+        default: 0
+    }
+});
+
+const Candidate = mongoose.model('Candidate', candidateSchema);
+
+
 
 
 
