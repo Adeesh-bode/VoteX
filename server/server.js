b26cf8fc438f9e8626de127a574d33fb2367e074
adesh.exc <bodead@rknec.edu>
2024-05-29 02:36:19 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+// todo: make 3 roles in project , by auth admin  candidate , normal voter . Now only two
 // config-server.js , schema in models , routing in routes , controller not seperated
 const express = require('express'); // web framework for Node.js
 const mongoose = require('mongoose');
