799324ffcb41769bcf50dde537d3c28cb7e37d9c
adesh.exc <bodead@rknec.edu>
2024-06-03 02:37:54 -0700

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+// 2 roles; normal voter and adnin ( who can monitor as well as put name of candidates for election )
 import React from 'react'
 import Hero from '../components/Hero'
 import Numbers from '../components/Numbers'
