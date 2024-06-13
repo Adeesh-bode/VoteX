a30254a06eb13ed81a0149c4434d5463683b7536
adesh.exc <bodead@rknec.edu>
2024-06-13 10:47:18 -0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/vote" element={<Vote />} />
           <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
     </Router>
   );
